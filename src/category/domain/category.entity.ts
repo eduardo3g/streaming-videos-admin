f60f1360b608952fcea56e2b8120a18475c200cb
Eduardo Santana <eduardosbrasil10@gmail.com>
2023-11-14 21:00:04 -0300

    fix(category/entity): fix toJSON method to include the UUID

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -51,7 +51,7 @@ export class Category {
 
   toJSON() {
     return {
-      category_id: this.category_id,
+      category_id: this.category_id.id,
       name: this.name,
       description: this.description,
       is_active: this.is_active,
