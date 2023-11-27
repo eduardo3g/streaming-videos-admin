e2015a5cfb06a1dd30acf8ddadfc20df3582aa29
Eduardo Santana <eduardosbrasil10@gmail.com>
2023-11-27 18:42:24 -0300

    fix: fix type on method name of Repository interface

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -2,7 +2,7 @@ import { Entity } from "../entity";
 import { ValueObject } from "../value-object";
 
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
-  intert(entity: E): Promise<void>;
+  insert(entity: E): Promise<void>;
   bulkInsert(entities: E[]): Promise<void>;
   update(entity: E): Promise<void>;
   delete(entity_id: EntityId): Promise<void>;
